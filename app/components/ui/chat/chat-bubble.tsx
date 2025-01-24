7b377269b78646e4ea18bea7ba262641af8f2b41
ashayden <ashayden@gmail.com>
2025-01-24 14:37:38 -0600

    feat: add avatar and button components

--- app/components/ui/chat/chat-bubble.tsx
+++ app/components/ui/chat/chat-bubble.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarImage, AvatarFallback } from "../avatar";
 import MessageLoading from "./message-loading";
 import { Button, ButtonProps } from "../button";
 
