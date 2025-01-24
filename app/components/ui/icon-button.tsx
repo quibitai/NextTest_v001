7f4927914175e7c7f648088794ac205682485eb8
ashayden <ashayden@gmail.com>
2025-01-24 14:55:37 -0600

    fix: update button import path in icon-button component

--- app/components/ui/icon-button.tsx
+++ app/components/ui/icon-button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, ButtonProps } from "@/components/ui/button";
+import { Button, ButtonProps } from "./button";
 import { cn } from "@/lib/utils";
 import { CSSProperties } from "react";
 
